130d988847d8c7ccdcfcc7c8470ffff6fae99feb
Calistus Obeke <howell2028@yahoo.com>
2020-05-15 00:54:14 +0100

    Comment out details

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const IndexPage = () => (
     <SEO title="Home" />
     {/* <Slide /> */}
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    {/* <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p> */}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
